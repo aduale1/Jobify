2a251029cff4a04a08fdc32f10e37bfe509265fb
Abdirahman Duale <ahduale7@gmail.com>
2024-09-03 22:06:47 -0600

    Initial commit

---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import { Link } from "react-router-dom";
+import { Logo } from "../components";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking </span>App
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam,
+            temporibus perferendis officiis eum molestiae soluta voluptatem
+            neque repellendus. Obcaecati velit atque ullam error ad suscipit?
+            Molestiae possimus ducimus odio ea?
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
---
+++ controllers/jobController.js
@@ -0,0 +1,35 @@
+import Job from "../models/JobModel.js";
+import { StatusCodes } from "http-status-codes";
+
+export const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({ createdBy: req.user.userId });
+  res.status(StatusCodes.OK).json({ jobs });
+};
+
+export const createJob = async (req, res) => {
+  req.body.createdBy = req.user.userId;
+  //const { company, position } = req.body;
+  const job = await Job.create(req.body);
+  // Whenever you create a resource, the status code should be 201
+  res.status(StatusCodes.CREATED).json({ job });
+};
+
+export const getJob = async (req, res) => {
+  const job = await Job.findById(req.params.id);
+  res.status(StatusCodes.OK).json({ job });
+};
+
+export const updateJob = async (req, res) => {
+  const updatedJob = await Job.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(StatusCodes.OK).json({ msg: "job modified", job: updatedJob });
+};
+
+export const deleteJob = async (req, res) => {
+  const removedJob = await Job.findByIdAndDelete(req.params.id);
+  console.log(removedJob);
+
+  res.status(StatusCodes.OK).json({ msg: "job deleted", job: removedJob });
+};
