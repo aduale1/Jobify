22778563dec58532627c932b5b87aaa69a287c30
Abdirahman Duale <ahduale7@gmail.com>
2025-06-23 15:43:03 -0600

    Add AllJobs page layout and context setup; create JobsContainer and Jobs component

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -3,6 +3,7 @@ import { JobsContainer, SearchContainer } from "../components";
 import customFetch from "../utils/customFetch";
 import { useLoaderData } from "react-router-dom";
 import { useContext, createContext } from "react";
+import { use } from "react";
 
 export const loader = async () => {
   try {
@@ -13,14 +14,19 @@ export const loader = async () => {
     return error;
   }
 };
+
+const AllJobsContext = createContext();
+
 const AllJobs = () => {
   const { data } = useLoaderData();
 
   return (
-    <>
+    <AllJobsContext.Provider value={{ data }}>
       <SearchContainer />
       <JobsContainer />
-    </>
+    </AllJobsContext.Provider>
   );
 };
+
+export const useAllJobsContext = () => useContext(AllJobsContext);
 export default AllJobs;
